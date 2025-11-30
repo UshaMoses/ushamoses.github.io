e6b49c5037ccee6a8004a0be6a03a105f59ce779
UshaMoses <usha.moses@dcmail.ca>
2025-11-30 17:11:06 -0500

    created a ball class
    defined the class, the constructor and the method to draw, update, and collision detection

--- assignment4/part3/javascript.js
+++ assignment4/part3/javascript.js
@@ -24,3 +24,48 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
 }
+// ----------- BALL CLASS -----------
+class Ball extends Shape {
+  constructor(x, y, velX, velY, size, color) {
+    super(x, y, velX, velY);
+    this.size = size;
+    this.color = color;
+    this.exists = true; // NEW
+  }
+
+  draw() {
+    if (!this.exists) return;
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  update() {
+    if (!this.exists) return;
+
+    if (this.x + this.size >= width || this.x - this.size <= 0) {
+      this.velX = -this.velX;
+    }
+    if (this.y + this.size >= height || this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (!(this === ball) && ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+}
