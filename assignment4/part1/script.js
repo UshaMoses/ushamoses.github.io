e3eb6851d21c730fbb8dfb2723568fd9883b9fc8
UshaMoses <usha.moses@dcmail.ca>
2025-11-30 09:05:42 -0500

    added eventlistener and partial generate function

--- assignment4/part1/script.js
+++ assignment4/part1/script.js
@@ -26,3 +26,9 @@ function randomValueFromArray(array) {
 
 function returnRandomStoryString() {
   // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day
+  return storyText;
+}
+
+// Event listener and partial generate function definition
+
+generateBtn.addEventListener("click", generateStory); 
