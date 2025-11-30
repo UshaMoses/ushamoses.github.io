6e01fe5c3902a4f531da0e7c21f3bb060cd70190
UshaMoses <usha.moses@dcmail.ca>
2025-11-30 09:03:40 -0500

    added function returnRnadomStoryString

--- assignment4/part1/script.js
+++ assignment4/part1/script.js
@@ -9,4 +9,20 @@ function randomValueFromArray(array) {
   return array[random];
 }
 
-/
+// Father Christmas// Raw text strings
+
+// Willy the Goblin
+// Big Daddy
+
+// the soup kitchen
+// Disneyland
+// the White House
+
+// spontaneously combusted
+// melted into a puddle on the sidewalk
+// turned into a slug and slithered away
+
+// Partial return random string function
+
+function returnRandomStoryString() {
+  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day
