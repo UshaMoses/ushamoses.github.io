3c522c762c1ea33b36b21df16011b41eeed287fb
UshaMoses <usha.moses@dcmail.ca>
2025-11-30 08:50:27 -0500

    Update script.js

--- assignment4/part1/script.js
+++ assignment4/part1/script.js
@@ -9,43 +9,4 @@ function randomValueFromArray(array) {
   return array[random];
 }
 
-// Raw text strings
-
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
-
-// the soup kitchen
-// Disneyland
-// the White House
-
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
-
-// Partial return random string function
-
-function returnRandomStoryString() {
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
-
-  return storyText;
-}
-
-// Event listener and partial generate function definition
-
-generateBtn.addEventListener("click", generateStory);
-
-function generateStory() {
-  if (customName.value !== "") {
-    const name = customName.value;
-  }
-
-  if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature = Math.round(94);
-  }
-
-  // TODO: replace "" with the correct expression
-  story.textContent = "";
-  story.style.visibility = "visible";
-}
+/
