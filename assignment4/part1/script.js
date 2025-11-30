1f5ff080c8c8ecb4b0ec5f7a63712378d36e8eae
UshaMoses <usha.moses@dcmail.ca>
2025-11-30 09:28:30 -0500

    declared and added the story text

--- assignment4/part1/script.js
+++ assignment4/part1/script.js
@@ -25,7 +25,7 @@ const events = ["spontaneously combusted", "melted into a puddle on the sidewalk
 
 function returnRandomStoryString() {
   const randomCharacter = randomValueFromArray(characters);
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day
+  let storyText It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day
   return storyText;
 }
 
