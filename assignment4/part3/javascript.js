cbbce712052fbc4ee6c7988afceec2c86da709c3
UshaMoses <usha.moses@dcmail.ca>
2025-11-30 17:04:40 -0500

    created a super class for shape functions for random color and number

--- assignment4/part3/javascript.js
+++ assignment4/part3/javascript.js
@@ -4,3 +4,23 @@ const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
+
+// ----------- SHAPE SUPER CLASS -----------
+class Shape {
+  constructor(x, y, velX, velY) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+  }
+  }
+
+// Utility — Random number
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Utility — Random color
+function randomRGB() {
+  return `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+}
