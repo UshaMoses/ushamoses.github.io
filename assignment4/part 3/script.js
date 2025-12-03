7b06447046d58e69aa865801b4fb24c336ee5964
UshaMoses <usha.moses@dcmail.ca>
2025-12-03 17:50:31 -0500

    function to generate random number

--- assignment4/part 3/script.js
+++ assignment4/part 3/script.js
@@ -6,3 +6,9 @@ const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
