19b560a3fe69fa2b6762136d327f304eac644918
UshaMoses <usha.moses@dcmail.ca>
2025-12-03 18:47:27 -0500

    Update main.js

--- assignment4/part 3/main.js
+++ assignment4/part 3/main.js
@@ -17,6 +17,7 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+// ball class
 class Ball {
   constructor(x, y, velX, velY, color, size) {
     this.x = x;
@@ -27,3 +28,59 @@ class Ball {
     this.size = size;
   }
 }
+draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+  update() {
+    if (this.x + this.size >=  width || this.x - this.size <= 0) {
+      this.velX = -this.velX;
+    } 
+    if (this.y + this.size >= height || this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball){ 
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+
+  // Create Balls
+const balls = [];
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),  
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+  balls.push(ball);
+}
+//Animation loop
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+  requestAnimationFrame(loop);
+}
+
+loop();
